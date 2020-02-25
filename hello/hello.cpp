649f49a3a16bd248e805ad9b5f828f85ad848b03
James Rees <James.rees@outlook.com>
2020-02-25 21:19:24 +0900

    Added .vscode files for building and debugging a sample hello world application

---
+++ hello/hello.cpp
@@ -0,0 +1,11 @@
+/* hello.cpp */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test = 5;
+    cout << "Test is: " << test << endl;
+    cout << "Hello, world" << endl;
+    return 0;
+}
